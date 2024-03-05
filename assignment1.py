08b0bcea8032f7b9eb83a6fa2e993dce0dde1ca7
hakeemfaider <154809165+hakeemfaider@users.noreply.github.com>
2024-03-05 14:35:00 +1300

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,25 @@
+from functions_minesweeper import *
+#TASK 1
+#board = initialise_board()
+#print(board)
+
+#TASK 2
+#display_board(board)
+
+#TASK 3
+#positions = [[1,3], [3,1]]
+#insert_mines(board, positions)
+#display_board(board)
+
+#TASK 4
+#count_adjacent_mines(board, 0, 0)
+
+#TASK 5
+#play_turn_board(board, 3, 1)
+#display_board(board)
+
+#TASK 6
+#print(check_win(board))
+
+#TASK 7
+play_game([[1,3], [3,1]])
